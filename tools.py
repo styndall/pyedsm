eced8c6735b41ea5968ef1d386bfcb393a813013
Stephen Tyndall <stephen.tyndall@nokia.com>
2020-08-24 23:31:10 -0400

    fleshing out a bit

--- tools.py
+++ tools.py
@@ -1,9 +1,20 @@
 import requests
 
 
-BASE = "https://www.edsm.net/api-v1/"
-
+BASE = "https://www.edsm.net/api-v1/system?systemName="
+COORDS = "&showCoordinates=1"
+STAR_TYPE = "&showPrimaryStar=1"
+INFO = "&showInformation=1"
+PERMIT = "&showPermit=1"
 
 def get_location(system_name):
-    call = BASE + "system?systemName=" + system_name + "&showCoordinates=1"
+    call = BASE + system_name + COORDS
     return requests.get(call).json().get("coords")
+
+def get_star_type(system_name):
+    call = BASE + system_name + STAR_TYPE
+    return requests.get(call).json().get("primaryStar").get("type")
+
+def get_info(system_name):
+    call = BASE + system_name + INFO
+    return requests.get(call).json().get("information")
